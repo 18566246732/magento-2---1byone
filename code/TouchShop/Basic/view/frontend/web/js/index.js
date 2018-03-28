ce6365e6e5f463dfea7e2f07661520a60e33ee43
baozi <18566246732@163.com>
2018-03-28 12:05:23 +0800

    fix login redirect issue

--- code/TouchShop/Basic/view/frontend/web/js/index.js
+++ code/TouchShop/Basic/view/frontend/web/js/index.js
@@ -1,6 +1,23 @@
 (function () {
     $j = jQuery.noConflict();
     $j(document).ready(function () {
+        const addCookie = function (name, value, expiresHours) {
+            var cookieString = name + "=" + escape(value);
+            //判断是否设置过期时间
+            if (expiresHours > 0) {
+                var date = new Date();
+                date.setTime(date.getTime + expiresHours * 3600 * 1000);
+                cookieString = cookieString + "; expires=" + date.toGMTString();
+            }
+            document.cookie = cookieString;
+        }
+
+        addCookie("login_redirect", <?php echo ?>)
+
+        // var cookieString = "login_redirect =" + location.href
+        //
+        // document.cookie += cookieString;
+
 
 
     });
