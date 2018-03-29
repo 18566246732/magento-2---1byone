13772db2b6060eea1f1de5a7845f51e412271187
baozi <18566246732@163.com>
2018-03-29 17:04:37 +0800

    useing jquery by requirejs to fix swatch color not working problem

--- design/frontend/TouchShop/Peanut/TouchShop_Basic/requirejs-config.js
+++ design/frontend/TouchShop/Peanut/TouchShop_Basic/requirejs-config.js
@@ -3,19 +3,19 @@ var config = {
         'owlcarousel': "TouchShop_Basic/js/owl.carousel.min",
         'twbsPagination' : "TouchShop_Basic/js/jquery.twbsPagination.min",
         'pagination' : "TouchShop_Basic/js/jquery.simplePagination",
-        'bootstrap' : "TouchShop_Basic/js/bootstrap.min.js"
+        'bootstrap' : "TouchShop_Basic/js/bootstrap.min",
     },
     shim: {
         'owlcarousel': {
+            deps: ['jquery', "bootstrap"]
+        },
+        'bootstrap' : {
             deps: ['jquery']
         },
         'twbsPagination' : {
-            deps: ['jquery', 'bootstrap']
-        },
-        'pagination' : {
             deps: ['jquery']
         },
-        'bootstrap' : {
+        'pagination' : {
             deps: ['jquery']
         }
     }
