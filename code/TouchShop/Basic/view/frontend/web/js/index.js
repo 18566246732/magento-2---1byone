ae6d2d5bb26fdad56866e9d65e700c5845654b62
baozi <18566246732@163.com>
2018-04-10 14:42:50 +0800

    add dotdotdot plugin to fix multiline ignore issue

--- code/TouchShop/Basic/view/frontend/web/js/index.js
+++ code/TouchShop/Basic/view/frontend/web/js/index.js
@@ -1,19 +1,23 @@
 (function () {
-    require(['jquery'], function ($) {
+    require(['jquery', 'dotdotdot'], function ($) {
         $(function () {
             var proItem = $(".product-items .product-item");
             var reviewsSummary = $(".product-reviews-summary.short");
-            var proItemLink = $(".product-items .product-item .product-item-link");
+            // var proItemLink = $(".product-items .product-item .product-item-link");
+            var proItemLink = $(".product-item-link");
             proItem.each(function (index, e) {
                 if($(e).find(".product-reviews-summary").length === 0) {
                     $(e).find(".product-item-name").css("padding-bottom", "25px");
                 }
             });
             proItemLink.each(function (index, e) {
-                if($(e).text().length >= (60 + 78)) {
-                    $(e).parent().addClass("p-i-n");
-                }
-            })
+                // console.log("e.txt.length:", $(e).text().trim().length);
+                // $clamp(e,{clamp: 2});
+                $(e).dotdotdot();
+                // if($(e).text().length >= (60 + 78)) {
+                //     $(e).parent().addClass("p-i-n");
+                // }
+            });
         })
     })
 })()
