949e3409b80edbf69663d52ff41a9c3e566f5956
baozi <18566246732@163.com>
2018-04-19 15:38:10 +0800

    fix account captcha, revise footer design

--- design/frontend/TouchShop/Peanut/Magento_Newsletter/web/js/subscribe.js
+++ design/frontend/TouchShop/Peanut/Magento_Newsletter/web/js/subscribe.js
@@ -1,8 +1,12 @@
 define(['jquery'], function ($) {
-    return function (baseUrl, urlArr) {
+    return function (baseUrl, urlArr, hoverUrlArr) {
         $('.iconfont').each(function (index, e) {
             $(e).css("background-image", 'url('+ baseUrl + urlArr[index]+')');
+            $(e).hover(function () {
+                $(this).css("background-image", 'url('+ baseUrl + hoverUrlArr[index] + ')');
+            }, function () {
+                $(this).css("background-image", 'url('+ baseUrl + urlArr[index] + ')');
+            });
         });
-        console.log("socials:", $(".socials"));
     }
 })
---
+++ docs/media-update/20180419-1132-footer-share-imgs/ReadMe.md
@@ -0,0 +1,1 @@
+copy imgs to pub/media/wysiwyg/ directory
