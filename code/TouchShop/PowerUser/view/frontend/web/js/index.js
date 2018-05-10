1afc8ab63780ce38237bb89a6abdafa69496dd66
baozi <18566246732@163.com>
2018-05-10 17:08:32 +0800

    fix faq-hint

--- code/TouchShop/PowerUser/view/frontend/web/js/index.js
+++ code/TouchShop/PowerUser/view/frontend/web/js/index.js
@@ -32,28 +32,28 @@ define(['uiComponent', 'ko'], function (Component, ko) {
             // var isVisible = false;
             this.data = ko.observableArray([
                     {
-                        title: 'Q: As a brand-new one, why do I join Super User?',
-                        description: 'Super User will have excusive discount codes and rewards.\n' +
-                        '    Except these,\n' +
-                        '    Super User usually will be the first one to know more about the latest product.\n' +
-                        '    Super User may change products of 1byone.',
+                        title: 'Q:As a brand-new one, why do I join Super User?',
+                        description: 'Super User will receive products from 1byone.<br/>' +
+                        'Super User will have excusive discount codes and rewards.<br/>' +
+                        'Except these,<br/>' +
+                        'Super User usually will be the first one to know more about the latest product.<br/>' +
+                        'Super User may change products of 1byone.',
                         isVisible: ko.observable(false),
                         degree: ko.observable('rotate(-45deg)')
 
                     },
                     {
                         title: 'Q: Who should apply as a Super User?',
-                        description: 'A: Everyone interested in 1byone and active in social media can apply to be a Super User.Some basic\n' +
-                        '    requirements for application:\n' +
-                        '    Social media— Minimum 20K followers and active engagement\n' +
-                        '    YouTube Channel—Minimum 1K subscribers and high-quality content\n' +
-                        '    Website / Forum / Blog— Minimum 1K visits daily',
+                        description: 'Everyone interested in 1byone and active in social media can apply to be a Super User.Some basic requirements for application:<br/>' +
+                        'Social media— Minimum 20K followers and active engagement<br/>' +
+                        'YouTube Channel—Minimum 1K subscribers and high-quality content<br/>' +
+                        'Website / Forum / Blog— Minimum 1K visits daily',
                         isVisible: ko.observable(false),
                         degree: ko.observable('rotate(-45deg)')
                     },
                     {
                         title: 'Q: I have a problem of Super User application?',
-                        description: 'A: If you have any problems with Super User, please send an email to <a>ushelp@1byone.com</a> and we\'ll reply back to\n' +
+                        description: 'If you have any problems with Super User, please send an email to <a>ushelp@1byone.com</a> and we\'ll reply back to\n' +
                         '    you within 3 work days.',
                         isVisible: ko.observable(false),
                         degree: ko.observable('rotate(-45deg)')
